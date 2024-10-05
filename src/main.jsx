d4ed6be8ffbdc4ffcd9fafcd21defc7d081b52ab
Albina Sabitova <albina.sabitovaa@icloud.com>
2024-10-05 15:47:40 +0200

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -4,12 +4,17 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+//import { store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
+import store, { persistor } from './redux/store.js'
+//import store from './redux/store.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
      <Provider store={store}>
-       <App />
+       <PersistGate persistor={persistor}>
+          <App />
+       </PersistGate>
      </Provider>
   </BrowserRouter>  
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import CartSlice from "./CartSlice";
+import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-export const store = configureStore({
-    reducer:{
-        cartProducts:CartSlice
-    }
-})
+const rootReducer = combineReducers({
+  cartProducts: CartSlice,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      })
+  });
+  
+export const persistor = persistStore(store);
+export default store
+
+// export const store = configureStore({
+//     reducer:{
+//         cartProducts:CartSlice
+//     }
+// })
