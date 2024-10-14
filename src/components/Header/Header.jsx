a30976fd3faf752ab53c256962df239d3ff6e440
Albina Sabitova <albina.sabitovaa@icloud.com>
2024-10-14 15:10:06 +0200

    m

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,36 +3,40 @@ import { MdOutlineShoppingCart } from "react-icons/md";
 import { NavLink } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { cartSelector } from "../../redux/CartSlice";
-import logo from '../../assets/image/logo.png'
-import cls from './Header.module.css'
+import logo from "../../assets/image/logo.png";
+import cls from "./Header.module.css";
 import { FaRegHeart } from "react-icons/fa";
 import { HiOutlineHome } from "react-icons/hi";
 import { RiLoginBoxLine } from "react-icons/ri";
 
-
 function Header() {
-    const {cart:cartProducts,favourite:cartFavourite} = useSelector(cartSelector) 
-    
+  const { cart: cartProducts, favourite: cartFavourite } =
+    useSelector(cartSelector);
+
   return (
-      <nav className={cls.header__nav}>
-           <div>
-          <NavLink className={cls.link__home} to="/"><HiOutlineHome className={cls.cart__icon}  /></NavLink>
-           </div>
-          <NavLink to="/"><img className={cls.logo} src={logo} alt="" /></NavLink>
-          <div className={cls.links__container}>
-              <NavLink to='register'>
-                 <RiLoginBoxLine className={cls.log__icon} />  
-              </NavLink>  
-              <NavLink to='favoriet' className={cls.icon__cart_container}>
-                <FaRegHeart className={cls.cart__icon} />
-                <p className={cls.count}>{cartFavourite.length}</p>
-              </NavLink>
-              <NavLink className={cls.icon__cart_container} to="cart">
-                 <MdOutlineShoppingCart className={cls.cart__icon} />
-                 <p className={cls.count}>{cartProducts.length}</p>
-              </NavLink>
-          </div>
-      </nav>
+    <nav className={cls.header__nav}>
+      <div>
+        <NavLink className={cls.link__home} to="/">
+          <HiOutlineHome className={cls.cart__icon} />
+        </NavLink>
+      </div>
+      <NavLink to="/">
+        <img className={cls.logo} src={logo} alt="" />
+      </NavLink>
+      <div className={cls.links__container}>
+        <NavLink to="register">
+          <RiLoginBoxLine className={cls.log__icon} />
+        </NavLink>
+        <NavLink to="favoriet" className={cls.icon__cart_container}>
+          <FaRegHeart className={cls.cart__icon} />
+          <p className={cls.count}>{cartFavourite.length}</p>
+        </NavLink>
+        <NavLink className={cls.icon__cart_container} to="cart">
+          <MdOutlineShoppingCart className={cls.cart__icon} />
+          <p className={cls.count}>{cartProducts.length}</p>
+        </NavLink>
+      </div>
+    </nav>
   );
 }
 
